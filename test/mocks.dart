b01674d06607413bb60a5d4f8ccc727f9e793969
Emmanuel <manuelxtrem@gmail.com>
2023-07-05 01:09:12 +0000

    add some tests

---
+++ test/mocks.dart
@@ -0,0 +1,80 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:note_taking_app/data/config_database.dart';
+import 'package:note_taking_app/data/note_database.dart';
+import 'package:note_taking_app/data/note_repository.dart';
+import 'package:note_taking_app/model/note.dart';
+
+class MockNotesDatabase extends Mock implements NotesDatabase {
+  @override
+  Iterable<Note> getNotes() {
+    return super
+        .noSuchMethod(Invocation.method(#getNotes, []), returnValue: const Iterable<Note>.empty());
+  }
+
+  @override
+  Future saveNote(Note? note) async {
+    return super.noSuchMethod(Invocation.method(#saveNote, [note]));
+  }
+
+  @override
+  Future addNote(Note? note) async {
+    return super.noSuchMethod(Invocation.method(#addNote, [note]));
+  }
+
+  @override
+  Future deleteNote(Note? note) async {
+    return super.noSuchMethod(Invocation.method(#deleteNote, [note]));
+  }
+
+  @override
+  bool hasData() {
+    return super.noSuchMethod(Invocation.method(#hasData, []), returnValue: false);
+  }
+
+  @override
+  Future<Iterable<Note>> getUnsyncedNotes({ConfigDatabase? configDatabase}) async {
+    return super.noSuchMethod(Invocation.method(#getUnsyncedNotes, [configDatabase]),
+        returnValue: const Iterable<Note>.empty());
+  }
+}
+
+class MockConfigDatabase extends Mock implements ConfigDatabase {}
+
+class MockNotesRepository extends Mock implements NotesRepository {
+  @override
+  Future<void> syncNotes({
+    NotesDatabase? notesDatabase,
+    ConfigDatabase? configDatabase,
+    Connectivity? connectivity,
+    bool? freshInstall,
+  }) async {
+    return super.noSuchMethod(
+      Invocation.method(#syncNotes, [notesDatabase, configDatabase, connectivity, freshInstall]),
+    );
+  }
+
+  @override
+  Future<void> syncOfflineNotes(
+      {NotesDatabase? notesDatabase, ConfigDatabase? configDatabase}) async {
+    return super.noSuchMethod(
+      Invocation.method(#syncOfflineNotes, [notesDatabase, configDatabase]),
+    );
+  }
+
+  @override
+  Future<void> syncOnlineNotes({NotesDatabase? notesDatabase, bool? freshInstall}) async {
+    return super.noSuchMethod(
+      Invocation.method(#syncOfflineNotes, [notesDatabase, freshInstall]),
+    );
+  }
+}
+
+class MockConnectivity extends Mock implements Connectivity {
+  @override
+  Future<ConnectivityResult> checkConnectivity() async {
+    return super.noSuchMethod(Invocation.method(#checkConnectivity, []),
+        returnValue: ConnectivityResult.mobile);
+  }
+}
---
+++ test/notes_bloc_test.dart
@@ -0,0 +1,105 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:note_taking_app/bloc/notes_bloc.dart';
+import 'package:note_taking_app/model/note.dart';
+
+import 'mocks.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  group('NotesBloc', () {
+    late NotesBloc notesBloc;
+    late MockNotesDatabase mockNotesDatabase;
+    late MockConfigDatabase mockConfigDatabase;
+    late MockNotesRepository mockNotesRepository;
+    late MockConnectivity mockConnectivity;
+
+    final sampleNote = Note(
+      id: 'fake_uuid',
+      title: 'Test Note 1',
+      body: 'Some interesting detail about a note that is hard to write.',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+
+    setUp(() {
+      mockNotesDatabase = MockNotesDatabase();
+      mockConfigDatabase = MockConfigDatabase();
+      mockNotesRepository = MockNotesRepository();
+      mockConnectivity = MockConnectivity();
+
+      notesBloc = NotesBloc(
+        notesDatabase: mockNotesDatabase,
+        configDatabase: mockConfigDatabase,
+        notesRepository: mockNotesRepository,
+        connectivity: mockConnectivity,
+      );
+    });
+
+    tearDown(() {
+      notesBloc.close();
+    });
+
+    test('initial state is NotesInitialState', () {
+      expect(notesBloc.state, isA<NotesInitialState>());
+    });
+
+    test('emits NotesListState after GetAllNotesEvent', () async {
+      final expectedState = NotesListState([sampleNote]);
+
+      when(mockNotesDatabase.getNotes()).thenReturn([sampleNote]);
+      notesBloc.add(GetAllNotesEvent());
+
+      await expectLater(notesBloc.stream, emits(expectedState));
+    });
+
+    test('emits NotesSavedState after SaveNoteEvent', () async {
+      final note = Note(
+        id: 'fake_uuid',
+        title: 'Test Note 1',
+        body: 'Some interesting detail about a note that is hard to write.',
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      );
+      final expectedState = NotesSavedState(note);
+
+      notesBloc.add(SaveNoteEvent(note));
+
+      await expectLater(notesBloc.stream, emits(expectedState));
+    });
+
+    test('emits NotesSavedState after AddNoteEvent', () async {
+      final note = Note(
+        id: 'fake_uuid',
+        title: 'Test Note 1',
+        body: 'Some interesting detail about a note that is hard to write.',
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      );
+      final expectedState = NotesSavedState(note);
+
+      notesBloc.add(AddNoteEvent(note));
+
+      await expectLater(notesBloc.stream, emits(expectedState));
+    });
+
+    // Add more tests for other events and states
+
+    test('calls syncNotes method on SyncNotesEvent', () async {
+      const connectivityResult = ConnectivityResult.mobile;
+
+      when(mockConnectivity.checkConnectivity()).thenAnswer((_) async => connectivityResult);
+      when(mockNotesDatabase.getUnsyncedNotes(configDatabase: mockConfigDatabase))
+          .thenAnswer((_) async => [sampleNote]);
+
+      final expectedState = NotesSyncedState();
+
+      notesBloc.add(SyncNotesEvent());
+
+      await expectLater(notesBloc.stream, emits(expectedState));
+    });
+  });
+}
